3a1a71dcb2ad8777c1cfb8d76bfeebca21df0295
pacogomezarnal <fjgomez@florida-uni.es>
2015-01-27 06:36:41 +0100

    Uso de la camara

--- datos/src/main/java/com/dam/datos/DataBaseHelper.java
+++ datos/src/main/java/com/dam/datos/DataBaseHelper.java
@@ -1,42 +1,76 @@
 package com.dam.datos;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
+    //Datos de la tabla
+    final private static String NAME = "artistas_db";
     final static String TABLE_NAME = "artistas";
-    final static String ARTIST_NAME = "nombre";
-    final static String ID = "_id";
-    final static String[] columns = { ID, ARTIST_NAME };
 
+    final static String ID = "_id";
+    final static String ARTIST_NAME = "nombre";
+
+    //Comandos
+    final static String[] columns = { ID, ARTIST_NAME };
     final private static String CREATE_CMD =
 
-            "CREATE TABLE artistas (" + ID
+            "CREATE TABLE "+ TABLE_NAME +" (" + ID
                     + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                     + ARTIST_NAME + " TEXT NOT NULL)";
 
-    final private static String NAME = "artistas_db";
+
     final private static Integer VERSION = 1;
     final private Context mContext;
 
+    //Constructor
     public DataBaseHelper(Context context) {
         super(context, NAME, null, VERSION);
         this.mContext = context;
     }
 
+    //Creación de la base de datos
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //Creamos la base de datos
+        Log.i(this.getClass().toString(), "Tabla ARTISTAS creada");
         db.execSQL(CREATE_CMD);
+        //La rellenamos
+        ContentValues values = new ContentValues();
+
+        values.put(DataBaseHelper.ARTIST_NAME, "Vetusta Morla");
+        db.insert(DataBaseHelper.TABLE_NAME, null, values);
+        values.clear();
+
+        values.put(DataBaseHelper.ARTIST_NAME, "ColdPlay");
+        db.insert(DataBaseHelper.TABLE_NAME, null, values);
+        values.clear();
+
+        values.put(DataBaseHelper.ARTIST_NAME, "All India Radio");
+        db.insert(DataBaseHelper.TABLE_NAME, null, values);
+        Log.i(this.getClass().toString(), "Datos insertados");
     }
 
+    //Actualización de la base de datos
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // N/A
     }
 
+    //Borrando de la base de datos
     void deleteDatabase() {
         mContext.deleteDatabase(NAME);
+    }
+
+    //Lectura de la base de datos
+    public  Cursor readArtistas(SQLiteDatabase db) {
+        return db.query(TABLE_NAME,
+                columns, null, new String[] {}, null, null,
+                null);
     }
 }
--- datos/src/main/java/com/dam/datos/MainActivity.java
+++ datos/src/main/java/com/dam/datos/MainActivity.java
@@ -12,11 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 
 public class MainActivity extends ListActivity {
-    private SQLiteDatabase mDB = null;
+
     private DataBaseHelper mDbHelper;
+    private SQLiteDatabase db;
     private SimpleCursorAdapter mAdapter;
 
     private static final String TAG = "Datos";
@@ -27,94 +29,20 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // CReamos un nuevo DataBase
+        // Creamos una nueva DataBase
         mDbHelper = new DataBaseHelper(this);
-        // Abrimos la DB para su escritura
-        mDB = mDbHelper.getWritableDatabase();
+        db = mDbHelper.getWritableDatabase();
 
-        // borramos todos los registros para nuestro ejemplo
-        clearAll();
-        // reseteamos los registros
-        insertArtists();
 
-        //Leemos los registros
-        Cursor c = readArtists();
-        int columnas_layout[]={ R.id._id, R.id.nombre };
+        //Leemos la base de datos y mostramos la informacion
+        Cursor c = mDbHelper.readArtistas(db);
         mAdapter = new SimpleCursorAdapter(this, R.layout.list_layout, c,
-                DataBaseHelper.columns,columnas_layout ,
-               0);
-        //Los mostramos por pantalla
+                DataBaseHelper.columns, new int[] { R.id._id, R.id.nombre },
+                0);
         setListAdapter(mAdapter);
 
-        //Insertamos las acciones del boton
-        Button fixButton = (Button) findViewById(R.id.fix_button);
-        fixButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // arreglamos los campos erroneos
-                Log.v(TAG, "Arreglando");
-                fix();
-                Log.v(TAG, "Actualizando");
-                // Repintamos los datos
-                mAdapter.notifyDataSetChanged();
-            }
-        });
-
     }
 
-
-
-    // Borrar todos los registros
-    private void clearAll() {
-        mDB.delete(DataBaseHelper.TABLE_NAME, null, null);
-    }
-
-    // Insert several artist records
-    private void insertArtists() {
-
-        ContentValues values = new ContentValues();
-
-        values.put(DataBaseHelper.ARTIST_NAME, "Frank Sinatra");
-        mDB.insert(DataBaseHelper.TABLE_NAME, null, values);
-        values.clear();
-
-        values.put(DataBaseHelper.ARTIST_NAME, "Lady Gaga");
-        mDB.insert(DataBaseHelper.TABLE_NAME, null, values);
-        values.clear();
-
-        values.put(DataBaseHelper.ARTIST_NAME, "Jawny Cash");
-        mDB.insert(DataBaseHelper.TABLE_NAME, null, values);
-        values.clear();
-
-        values.put(DataBaseHelper.ARTIST_NAME, "Ludwig von Beethoven");
-        mDB.insert(DataBaseHelper.TABLE_NAME, null, values);
-    }
-
-    // Devuelve los registros de la base de datos
-    private Cursor readArtists() {
-        return mDB.query(DataBaseHelper.TABLE_NAME,
-                DataBaseHelper.columns, null, new String[] {}, null, null,
-                null);
-    }
-
-    // Modify the contents of the database
-    private void fix() {
-
-        // Borrando
-        mDB.delete(DataBaseHelper.TABLE_NAME,
-                DataBaseHelper.ARTIST_NAME + "=?",
-                new String[] { "Lady Gaga" });
-
-        // Actualizando
-        ContentValues values = new ContentValues();
-        values.put(DataBaseHelper.ARTIST_NAME, "Johnny Cash");
-
-        mDB.update(DataBaseHelper.TABLE_NAME, values,
-                DataBaseHelper.ARTIST_NAME + "=?",
-                new String[] { "Jawny Cash" });
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -130,7 +58,6 @@ public class MainActivity extends ListActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -138,9 +65,7 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onDestroy() {
 
-        mDB.close();
         mDbHelper.deleteDatabase();
-
         super.onDestroy();
 
     }
